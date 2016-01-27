41a5e7360fbeb321512849d01ca36f7e73c156de
Mark Pinese <mark.pinese@gmail.com>
2016-01-28 08:33:29 +1100

    More tracing code.

--- lenssim/lenssim.jl
+++ lenssim/lenssim.jl
@@ -1,40 +1,72 @@
-type Point
-	x::Float64
-	y::Float64
+import Cairo
+
+immutable type Point
+	x::AbstractFloat
+	y::AbstractFloat
 end
 +(p1::Point, p2::Point) = Point(p1.x + p2.x, p1.y + p2.y)
 -(p1::Point, p2::Point) = Point(p1.x - p2.x, p1.y - p2.y)
-*(p::Point, k::Float64) = Point(p.x*k, p.y*k)
+*(p::Point, k::AbstractFloat) = Point(p.x*k, p.y*k)
 norm(p::Point) = sqrt(p.x^2 + p.y^2)
 
+# p is the location of the intersection.  theta is the angle the intersecting ray makes with the intersected surface normal.
 type Intersection
 	p::Point
-	theta::Float64
+	theta::AbstractFloat
 end
 
-type Ray
-	source::Point
-	delta::Point
-end
-
+# p = source + k*delta, k unconstrained
 type Line
 	source::Point
 	delta::Point
 end
 
+# p = source + k*delta, k >= 0
+type Ray
+	source::Point
+	delta::Point
+end
+
+# p = source + k*delta, 0 <= k <= 1
+type Segment
+	source::Point
+	delta::Point
+end
+function draw(cr::CairoContext, e::circle)
+	Cairo.move_to(cr, e.source.x, e.source.y)
+	Cairo.line_to(cr, e.source.x + e.delta.x, e.source.y + e.delta.y)
+end
+
+# norm(p - centre) = radius
 type Circle
 	centre::Point
-	radius::Float64
+	radius::AbstractFloat
+end
+function draw(cr::CairoContext, e::circle)
+	Cairo.new_sub_path(cr)
+	Cairo.arc(cr, e.centre.x, e.centre.y, e.radius, 0, 2*pi)
+	Cairo.close_path(cr)
 end
 
+# index is the optical index of the current medium of the photon
 type Photon
 	ray::Ray
-	index::Float64
+	index::AbstractFloat
 end
 
-type CircularLens
-	boundary::Circle
-	index::Float64
+BoundaryGeometry = Union{Circle, Segment}
+
+# Note current setup cannot handle cemented groups.
+type OpticalElement
+	boundary::BoundaryGeometry
+	index::AbstractFloat
+end
+draw(cr::CairoContext, e::OpticalElement) = draw(cr, e.boundary)
+
+
+type System
+	elements::Array{OpticalElement, 1}
+	photons::Array{Array{Photon, 1}, 1}
 end
 
 
@@ -110,7 +142,29 @@ function intersect(r::Ray, l::Line)
 end
 
 
+function intersect(r::Ray, s::Segment)
+	line_int = intersect(Line(r.source, r.delta), Line(s.source, s.delta))
+
+	p0 = s.source
+	p1 = s.source + s.delta
+	filter!(i->((i.p.x - p0.x)*(i.p.x - p1.x) <= 0 && (i.p.y - p0.y)*(i.p.y - p1.y) <= 0), line_int)
+
+	return line_int
+end
+
+
 intersect(Ray(Point(-5,-5), Point(1,1)), Circle(Point(1,1), 3))
 intersect(Ray(Point(5,5), Point(1,1)), Circle(Point(1,1), 3))
 
-function refract(p::Photon, )
+function plotSystem(system::System)
+	c = Cairo.CairoRGBSurface(256, 256)
+	cr = Cairo.CairoContext(c)
+
+	Cairo.save(cr)
+	Cairo.set_source_rgb(cr, 0, 1, 0)
+	for element in system.elements
+		draw(cr, element)
+	end
+	Cairo.restore(cr)
+end
+

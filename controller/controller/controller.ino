8f03531db5ba19cc9cf643de72767797e8069fbe
Mark Pinese <mark.pinese@gmail.com>
2017-01-22 14:29:34 +1100

    Updated to Adafruit_ILI9341_STM library.

--- controller/controller/controller.ino
+++ controller/controller/controller.ino
@@ -1,29 +1,43 @@
-#include <SPI.h> 
-#include <ILI_SdSpi.h>
-#include <ILI_SdFatConfig.h>
-#include <ILI9341_due_gText.h>
-#include <ILI9341_due.h>
-#include "fonts\Arial14.h"
+#include <Adafruit_GFX_AS.h>
+#include <Adafruit_ILI9341_STM.h>
 
+#include "SPI.h"
+
+
+const uint8_t TFT_CS = PC15;
+const uint8_t TFT_DC = PC14;
 const uint8_t TFT_RST = PC13;
-const uint8_t TFT_DC =  PC14;
-const uint8_t TFT_CS =  PC15;
 
-ILI9341_due tft = ILI9341_due(TFT_CS, TFT_DC, TFT_RST);
-ILI9341_due_gText t1(&tft);
+
+Adafruit_ILI9341_STM tft = Adafruit_ILI9341_STM(TFT_CS, TFT_DC, TFT_RST);
+
+
+void send_display_diagnostics()
+{
+    Serial.println("ILI9341 diagnostics:");
+    Serial.print("Display Power Mode: 0x");
+    Serial.println(tft.readcommand8(ILI9341_RDMODE), HEX);
+    Serial.print("MADCTL Mode:        0x");
+    Serial.println(tft.readcommand8(ILI9341_RDMADCTL), HEX);
+    Serial.print("Pixel Format:       0x");
+    Serial.println(tft.readcommand8(ILI9341_RDPIXFMT), HEX);
+    Serial.print("Image Format:       0x");
+    Serial.println(tft.readcommand8(ILI9341_RDIMGFMT), HEX);
+    Serial.print("Self Diagnostic:    0x");
+    Serial.println(tft.readcommand8(ILI9341_RDSELFDIAG), HEX);   
+}
+
 
 void setup()
 {
   Serial.begin(115200);
   tft.begin();
-  tft.setRotation(iliRotation270);  
-  tft.fillScreen(ILI9341_BLACK);
-  t1.defineArea(0, 0, 320, 240);
-  t1.selectFont(Arial14);
 }
 
-void loop()
+
+void loop(void)
 {
-  delay(1000);
+    delay(1000);
+    send_display_diagnostics();
 }
 

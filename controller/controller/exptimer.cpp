8429058c17601a5fe991df303303e86f29682cf2
Mark Pinese <mark.pinese@gmail.com>
2017-01-26 00:21:33 +1100

    Started filling in Exposure class members.

--- controller/controller/exptimer.cpp
+++ controller/controller/exptimer.cpp
@@ -2,8 +2,8 @@
 #include "fix24.h"
 
 
-const fix24_t min_t = fix24_t(4.0);
-const fix24_t max_t = fix24_t(32.0);
+const fix24_t min_log2t = fix24_t(2.0);     // 4.0
+const fix24_t max_log2t = fix24_t(5.0);     // 32.0
 const fix24_t min_log2Phi = fix24_t(-4.0);  // 16/255
 const fix24_t max_log2Phi = fix24_t(0.0);   // 255/255
 const fix24_t min_2log2N = fix24_t(1.0);    // f/1.4
@@ -11,28 +11,98 @@ const fix24_t max_2log2N = fix24_t(5.0);    // f/32
 const fix24_t min_2log2s = fix24_t(8.0);    // 16 cm
 const fix24_t max_2log2s = fix24_t(14.0);   // 128 cm
 
+// Fundamental equation:
 // log2H + K = log2t - 2log2N + log2Phi - 2log2s
 
 
 fix24_t Exposure::change_log2H(fix24_t delta)
 {
-  
+  // Try to change log2Phi to effect the desired changed in log2H,
+  // leaving t constant.
+  // Only if log2Phi will go out of bounds, do we change t.
+  // If both log2Phi and t would go out of bounds, change nothing.
+
+  //    log2H + delta = log2t - 2log2N + log2Phi - 2log2s
+  // => log2Phi + log2t = log2H + delta + 2log2N + 2log2s
+  // So we need to distribute delta across log2Phi and log2t,
+  // with priority to changing log2Phi.
+
+  if (log2Phi + delta < min_log2Phi)
+  {
+    // Underflow.  Put what we can into log2Phi and carry the 
+    // rest for t.
+    delta -= (log2Phi - min_log2Phi);
+    log2Phi = min_log2Phi;
+  }
+  else if (log2Phi + delta > max_log2Phi)
+  {
+    // Overflow.  Put what we can into log2Phi and carry the 
+    // rest for t.
+    delta -= (max_log2Phi - log2Phi);
+    log2Phi = max_log2Phi;
+  }
+  else
+  {
+    // In bounds.
+    log2Phi += delta;
+    delta = 0.0;
+  }
+
+  if (log2t + delta < min_log2t)
+  {
+    // Underflow.  Put what we can into log2t, and return the rest.
+    delta -= (log2t - min_log2t);
+    log2t = min_log2t;
+  }
+  else if (log2t + delta > max_log2t)
+  {
+    // Overflow.  Put what we can into log2t, and return the rest.
+    delta -= (max_log2t - log2t);
+    log2t = max_log2t;
+  }
+  else
+  {
+    // In bounds.
+    log2t += delta;
+    delta = 0.0;
+  }
+
+  // Update log2H to match.
+  log2H = log2t - log2N2 + log2Phi - log2s2;
+
+  return delta;
 }
 
 
 fix24_t Exposure::change_t(fix24_t delta)
 {
-  
-}
+  // Try to change log2Phi to effect the desired change in t, 
+  // leaving H constant.
+  // If log2Phi will go out of bounds, don't change anything.
 
-
-fix24_t Exposure::change_log2Phi(fix24_t delta)
-{
   if (log2Phi + delta < min_log2Phi)
+  {
+    // Underflow.  Put what we can into log2Phi, and return the rest.
+    delta -= (log2Phi - min_log2Phi);
     log2Phi = min_log2Phi;
+  }
   else if (log2Phi + delta > max_log2Phi)
+  {
+    // Overflow.  Put what we can into log2Phi, and return the rest.
+    delta -= (max_log2Phi - log2Phi);
     log2Phi = max_log2Phi;
-    
+  }
+  else
+  {
+    // In bounds.
+    log2Phi += delta;
+    delta = 0.0;
+  }
+
+  // Update log2t to match.
+  log2t = log2H + log2N2 - log2Phi + log2s2;
+
+  return delta;
 }
 
 
@@ -43,36 +113,6 @@ fix24_t Exposure::set_N(fix24_t new_N)
 
 
 fix24_t Exposure::set_s(fix24_t new_s)
-{
-  
-}
-
-
-fix24_t Exposure::get_H()
-{
-  
-}
-
-
-fix24_t Exposure::get_t()
-{
-  
-}
-
-
-fix24_t Exposure::get_Phi()
-{
-  
-}
-
-
-fix24_t Exposure::get_N()
-{
-  
-}
-
-
-fix24_t Exposure::get_s()
 {
   
 }
--- controller/controller/exptimer.h
+++ controller/controller/exptimer.h
@@ -20,16 +20,15 @@ private:
 public:
   fix24_t change_log2H(fix24_t delta);
   fix24_t change_t(fix24_t delta);
-  fix24_t change_log2Phi(fix24_t delta);
 
   fix24_t set_N(fix24_t new_N);
   fix24_t set_s(fix24_t new_s);
 
-  fix24_t get_H();
-  fix24_t get_t();
-  fix24_t get_Phi();
-  fix24_t get_N();
-  fix24_t get_s();
+  fix24_t get_log2H()     { return log2H; };
+  fix24_t get_log2t()     { return log2t; };
+  fix24_t get_log2Phi()   { return log2Phi; };
+  fix24_t get_log2N()     { return log2N2 / fix24_t(2.0); };
+  fix24_t get_log2s()     { return log2s2 / fix24_t(2.0); };
 };
 
 
